e50ea8ce4b38afa7c4c6fddd1f43f4a6dc635acb
Tohid Heshmati <tohidfla@gmail.com>
2020-09-13 19:27:20 +0200

    init

---
+++ src/main/java/com/project/giftoo/GiftooApplication.java
@@ -0,0 +1,13 @@
+package com.project.giftoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/giftoo/GiftooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.giftoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
